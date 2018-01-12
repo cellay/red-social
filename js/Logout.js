9250de2c168bef1c0c1f48ca9554a4a6616d2938
cellay <32296232+cellay@users.noreply.github.com>
2018-01-12 07:22:32 -0500

    Actualizando botón cerrar sesión

---
+++ js/Logout.js
@@ -0,0 +1,19 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDSwdy8QBD6nkYB8f52EtJ8MDO18pYVxJ0",
+  authDomain: "socialnetwork-be86a.firebaseapp.com",
+  databaseURL: "https://socialnetwork-be86a.firebaseio.com",
+  projectId: "socialnetwork-be86a",
+  storageBucket: "socialnetwork-be86a.appspot.com",
+  messagingSenderId: "884615217617"
+};
+firebase.initializeApp(config);
+
+//Cerrando sesión
+$('#logout').on('click', function(event) {
+  firebase.auth().signOut().then(function() {
+    $(location).attr('href', 'index.html');
+  }).catch(function(error) {
+    // An error happened.
+  });
+});
