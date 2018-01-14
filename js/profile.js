2737db37f23171ccd699d50cd7af1f8f1e29bd0a
LizbethFP <lizbeth.fp@gmail.com>
2018-01-14 15:38:45 -0500

    agregando sección para mostrarse publicación de comentario

--- js/profile.js
+++ js/profile.js
@@ -29,9 +29,12 @@ function createPosts() {
     var timeContainer = '<span class="col-xs-1 col-sm-1 col-md-1">' + time + '</span>';
     // Crear contenedor de íconos
     var iconsWrapper = '<div class="icons-wrapper col-xs-push-1 col-xs-offset-6 col-sm-offset-8 col-sm-3 col-md-offset-8 col-md-3"><a class="heart" href="#"><img src="https://image.flaticon.com/icons/png/128/149/149217.png" alt="heart"></a><a class="colored-heart" href="#" hidden><img src="https://image.flaticon.com/icons/png/128/214/214309.png" alt="colored-heart"></a><a class="comment-bubble" href="#"><img src="../assets/images/chat.svg"></a></div>';
+    // Crear espacio para crear comentario
     var commentSide = '<div class="row comment-side-container" hidden><div class="col-xs-12 comment-side"><textarea name="name" class="col-xs-12 post-txtarea" id="comment-txtarea" placeholder="Comentario..."></textarea><button type="button" name="button" id="comment-btn">Comentar</button></div></div>' ;
+    // Crear espacio donde se mostrará Comentario
+    var commentDisplaySection = '<div class="row"><p class="col-xs-12 col-sm-12 col-md-12 comment-display-section"></p></div>';
     // Crear espacio para agregar publicación con botones de gustar y comentar
-    var postWrapper = '<div class="post-wrapper col-xs-12 col-md-12">' + pUserName + pPost + picLoad + '<div class="row">' + timeContainer + iconsWrapper + '</div>' + commentSide + '</div>';
+    var postWrapper = '<div class="post-wrapper col-xs-12 col-md-12">' + pUserName + pPost + picLoad + '<div class="row">' + timeContainer + iconsWrapper + '</div>' + commentSide + commentDisplaySection + '</div>';
     // Agregar un párrafo de publicacíon que aparecerá en el perfil
     $(postSection).find('div').first().before(postWrapper);
     // Limpiar contenido de textarea después de publicar
@@ -104,8 +107,9 @@ function createComments() {
     // Capturar valor del área de texto
     var commentTxtAreaValue = $('#comment-txtarea').val();
     // Crear párrafo de publicación
-    var pComment = '<p class="parrapgrah-comment">' + commentTxtAreaValue + '</p>';
+    var pComment = '<div class"row"><p class="parrapgrah-comment col-xs-12 ">' + commentTxtAreaValue + '</p></p>';
     $(commentDisplaySection).append(pComment);
+    $('#comment-txtarea').val('');
   });
 }
 
